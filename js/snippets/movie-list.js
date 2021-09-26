8b3c8a251508feecc0a525fcec6a4568af0dde4f
ZAYD ELIMRANI <plutorider47@gmail.com>
2021-09-26 21:39:40 +0100

    Movie scene finished 'finally'

--- js/snippets/movie-list.js
+++ js/snippets/movie-list.js
@@ -61,7 +61,6 @@ Snippet_Movie_List = (function(Snippet) {
 				}catch(err){}
 			}
 		},
-		
         
 		activate: function() {
 		},
@@ -80,62 +79,65 @@ Snippet_Movie_List = (function(Snippet) {
         
 		navigate: function(direction) {
 			var $nowEl = this.$el.find('.items .focus');
+			
             switch(direction)
 			{
 				case 'down' : 
-					//Focus.to(this.$el.find('.items').next().find('ul .item').first())
-					this.parentIsHovered += 1;
-					this.top -= 256.67;
-					this.$el.css('top',this.top);
-					this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).find('.active').removeClass('active');
-					$nowEl.addClass('active');
-					
-					this.transformX = 0;
-
-					if(!this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().find('.active').length)
+					if(this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).next().length)
 					{
-						Focus.to(this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().find('li').first());
-						this.transformX =0;
-						this.$el.find($nowEl[0].parentNode).css({transform:'translateX('+this.transformX+'px)'});
+						this.parentIsHovered += 1;
+						this.top -= 256.67;
+						this.$el.css('top',this.top);
+						this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).find('.active').removeClass('active');
+						$nowEl.addClass('active');
+						
+						this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).css('opacity',0);
+						if(!this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).next().find('.active').length)
+						{
+							Focus.to(this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).next().find('li').first());
+							this.transformX = 0;
+						}
+						else
+						{
+								this.transformX = -this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).next().find('li.active')[0].offsetLeft;
+								Focus.to(this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).next().find('li.active'));
+								this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).next().find('.active').removeClass('active');
+						}
 					}
-					else
-					{
-						Focus.to(this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).next().find('li').first());
-						this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().find('.active').removeClass('active');
-					}
-
 					break;
 				case 'up' : 
-				this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).find('.active').removeClass('active');
-					$nowEl.addClass('active');
-					if(!this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().find('.active').length)
+					if(this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().length)
 					{
-						Focus.to(this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().find('li').first());
-						this.transformX =0;
-						this.$el.find($nowEl[0].parentNode).css({transform:'translateX('+this.transformX+'px)'});
+						this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).find('.active').removeClass('active');
+						$nowEl.addClass('active');
+						this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().css('opacity',1);
+						if(!this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().find('.active').length)
+						{
+							Focus.to(this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().find('li').first());
+							this.transformX =0;
+						}
+						else
+						{
+							this.transformX = -this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().find('li.active')[0].offsetLeft;
+							Focus.to(this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().find('.active'));
+							this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().find('.active').removeClass('active');
+						}
+						
+						if(this.top<(this.parent.$el[0].offsetHeight/2))
+						{
+							this.parentIsHovered -= 1;
+							this.top += 256.67;
+							this.$el.css('top',this.top);
+						}
 					}
-					else
-					{
-						Focus.to(this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().find('.active'));
-						this.$el.find(this.$el.find($nowEl[0].parentNode)[0].parentNode).prev().find('.active').removeClass('active');
-					}
-					
-					if(this.top<(this.parent.$el[0].offsetHeight/2))
-					{
-						this.parentIsHovered -= 1;
-						this.top += 256.67;
-						this.$el.css('top',this.top);
-					}					
-					this.transformX = 0;
 					break;
 				case 'right' :
-					this.transformX -=158;
-					this.$el.find($nowEl[0].parentNode).css({transform:'translateX('+this.transformX+'px)'})
-					Focus.to($nowEl.next());
-					//console.log( this.$el.find('.items').first())
-					/*this.$el.find('.items').forEach(element => {
-							console.log(element);
-					}); */
+					if($nowEl.next().length)
+					{
+						this.transformX -=158;
+						this.$el.find($nowEl[0].parentNode).css({transform:'translateX('+this.transformX+'px)'})
+						Focus.to($nowEl.next());
+					}
 					break; 
 				case 'left' :
 					if(this.transformX === 0)
@@ -159,14 +161,32 @@ Snippet_Movie_List = (function(Snippet) {
 		},
 
 		onFocus:function($el){
+			clearTimeout(this.timeOut);
+			$('#scene-movie #trailer iframe').removeAttr("src");
+			$('#scene-movie #trailer iframe').hide();232
+			$('#scene-movie #trailer img').show();
 			var index = $el.attr('data-index');
-			console.log(this.$movies[index]);
+			var date = new Date();
+			date.setHours(this.$movies[index].data.info.duration.split(':')[0]);
+			date.setMinutes(this.$movies[index].data.info.duration.split(':')[1]);
+			var duration = "";
+			if(date.getHours()){duration+= date.getHours()+" h";}
+			if(date.getMinutes()){duration+= date.getMinutes()+" min";}
+
 			$('#scene-movie #filmName').text(this.$movies[index].name);
-			$('#scene-movie #filmYear').text(this.$movies[index].data.info.releasedate);
-			$('#scene-movie #filmDuration').text(this.$movies[index].data.info.duration);
+			$('#scene-movie #filmYear').text(new Date(this.$movies[index].data.info.releasedate).getFullYear());
+			$('#scene-movie #filmDuration').text(duration);
 			$('#scene-movie #filmRating').text(this.$movies[index].data.info.rating);
 			$('#scene-movie #filmDescription').text(this.$movies[index].data.info.plot);
 			$('#scene-movie #trailer img').attr('src',this.$movies[index].data.info.movie_image);
+			
+			
+			this.timeOut = setTimeout(()=>{
+				$('#scene-movie #trailer iframe').attr('src','https://www.youtube-nocookie.com/embed/'+this.$movies[index].data.info.youtube_trailer+'?controls=0&autoplay=1&mute=1');
+				$('#scene-movie #trailer img').hide();
+				$('#scene-movie #trailer iframe').show();
+			},3500);
+			
 		},
 
 		tmpFill:function(){
